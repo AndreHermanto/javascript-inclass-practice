50a4b38c5957351dd4047c49f9fc38e5b3a6aca5
Andre <andre.hermanto93@gmail.com>
2024-01-14 19:35:20 +1100

    practice

---
+++ index.js
@@ -0,0 +1,101 @@
+// Function 1: Print "Hello world!!" for n times
+function printHelloWorldNTimes(n) {
+
+}
+
+// Function 2: It accepts a number N, then it return sum a sequence of numbers up to N
+// Example if n=5, then it will return the sum of 1+2+3+4+5
+// if n=4, then it will return the sum of 1+2+3+4
+function sumSequenceUpToN(n) {
+
+}
+
+// Function 3: Print a square of size N using *
+// Example log
+//*** 
+//*** 
+//***
+function printSquare(n) {
+
+}
+  
+// Function 4: Print a triangle of N rows
+// Example log
+//*
+//**
+//***
+function printTriangle(n) {
+
+}
+
+//Function 5: reverse a string
+//Example
+// if str is "Hello World", then it should return "dlroW olleH"
+function reverseString(str) {
+
+}
+
+//Function 6: Function will accept an array, then find the largest number in array
+//Example
+//findLargestNumber([5,4,1,244,12])
+//will return 244
+function findLargestNumber(arrNum){
+
+}
+
+//Function 7: Function will accept an array of numbers, then return array of numbers sorted in ascending order
+//Example
+//sortedArr([3,4,1,2,5,6,5])
+//will return [1,2,3,4,5,5,6]
+function sortedArr(arrNum){
+
+}
+
+//Function 8: This function takes two arrays of the same length as input and returns a new array obtained by multiplying the corresponding elements of the input arrays. The resulting array reflects the element-wise multiplication of the original arrays.
+//Example
+//multiplyArrays([1,2,3,4],[2,3,4,5])
+//will return [2,6,12,20]
+function multiplyArrays(arr1, arr2){
+
+}
+
+//Function 9: This function will accept an array of string, then sort the array based on the string length
+//Example
+//sortedStrArr(["Hello World", "Good", "Gday mate"])
+//will return ["Good", "Gday mate", "Hello World"]
+function sortedStrArr(arrStr){
+
+}
+
+//Function 10: This function will accept a string of a word(all lowercase), then check if the string is palindrome
+//Example
+//isPalindrome("level")
+//will return true
+//isPalindrome("kayak")
+//will return true
+//isPalindrome("test")
+//will return false
+function isPalindrome(str){
+
+}
+
+console.log("Function 1:")
+printHelloWorldNTimes(3);
+console.log("Function 2:")
+console.log(sumSequenceUpToN(4));
+console.log("Function 3:")
+printSquare(3);
+console.log("Function 4:")
+printTriangle(5);
+console.log("Function 5:")
+console.log(reverseString("Hello World"));
+console.log("Function 6:")
+console.log(findLargestNumber([5,4,1,244,12]));
+console.log("Function 7:")
+console.log(sortedArr([3,4,1,2,5,6,5]));
+console.log("Function 8:")
+console.log(multiplyArrays([1,2,3,4],[2,3,4,5]));
+console.log("Function 9:")
+console.log(sortedStrArr(["Hello World", "Good", "Gday mate"]));
+console.log("Function 10:")
+console.log(isPalindrome("level"))
